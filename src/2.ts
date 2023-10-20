20c10f6b33cd671118ec4ce6b7c0f01cc938c5ea
Maksim Gulay <maksimgulaj@gmail.com>
2023-10-20 21:50:55 +0300

    ts practice

--- src/2.ts
+++ src/2.ts
@@ -14,24 +14,25 @@
 
 */
 
-class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
-
+class Employee { constructor(
+  public name: string,
+   private department: string,
+   protected salary: number,
+) {}
+  
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
 }
 
 class Manager extends Employee {
+  constructor(
+     name: string,
+    department: string,
+    salary: number,
+  ) {
+    super(name, department, salary + 10000);
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
 
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,56 @@
+class Key {
+    private signature: number;
+    constructor () {this.signature = Math.random();}
+    getSignature(): number {
+        return this.signature;
+    }
+};
+
+class Person {
+    key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    abstract openDoor (key: Key): void;
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log("wellcome")
+        } else {
+            console.log("Door is close")
+        }
+    }
+}
+
+class MyHouse  extends House {
+    
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log("Door is open")
+        } else {
+        console.log("Wrong key")
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
